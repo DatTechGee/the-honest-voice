aa837faaed8d26b841cede180e9a0a1321f7c5b6
is <dattechgee.com>
2026-03-30 07:39:14 +0100

    Disable social links and fix contact form mail routing

--- mail/contact.js
+++ mail/contact.js
@@ -10,44 +10,33 @@ $(function () {
             var email = $("input#email").val();
             var subject = $("input#subject").val();
             var message = $("textarea#message").val();
+            var $this = $("#sendMessageButton");
+            var recipientEmail = "charityyohana201@gmail.com";
+            var mailSubject = encodeURIComponent(subject + " - from " + name);
+            var mailBody = encodeURIComponent(
+                "Name: " + name + "\n" +
+                "Email: " + email + "\n\n" +
+                message
+            );
 
-            $this = $("#sendMessageButton");
             $this.prop("disabled", true);
 
-            $.ajax({
-                url: "contact.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    email: email,
-                    subject: subject,
-                    message: message
-                },
-                cache: false,
-                success: function () {
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                            .append('</div>');
-                    $('#contactForm').trigger("reset");
-                },
-                error: function () {
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
-                    $('#success > .alert-danger').append('</div>');
-                    $('#contactForm').trigger("reset");
-                },
-                complete: function () {
-                    setTimeout(function () {
-                        $this.prop("disabled", false);
-                    }, 1000);
-                }
-            });
+            // Open the visitor's default email app with pre-filled details.
+            window.location.href = "mailto:" + recipientEmail + "?subject=" + mailSubject + "&body=" + mailBody;
+
+            $('#success').html("<div class='alert alert-success'>");
+            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    .append("</button>");
+            $('#success > .alert-success')
+                    .append("<strong>Your email app has been opened. Please send the draft message to complete your contact request.</strong>");
+            $('#success > .alert-success')
+                    .append('</div>');
+
+            $('#contactForm').trigger("reset");
+
+            setTimeout(function () {
+                $this.prop("disabled", false);
+            }, 1000);
         },
         filter: function () {
             return $(this).is(":visible");
